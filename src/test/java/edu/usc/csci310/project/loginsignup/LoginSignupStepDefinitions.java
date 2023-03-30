b8120780e9e02dcfa80e7256fc9ecab008fff5fc
Monil Sorathia <monilsorathia01@gmail.com>
2023-03-29 20:26:34 -0700

    added acceptance tests

--- src/test/java/edu/usc/csci310/project/loginsignup/LoginSignupStepDefinitions.java
+++ src/test/java/edu/usc/csci310/project/loginsignup/LoginSignupStepDefinitions.java
@@ -7,6 +7,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -23,6 +24,13 @@ public class LoginSignupStepDefinitions {
     public static void beforeAll() {
         System.out.println("Setting Up Driver");
         WebDriverManager.chromedriver().setup();
+        //WebDriverManager.chromedriver().setup();
+    }
+
+    @After
+    public void After()
+    {
+        driver.quit();
     }
 
     @Before
