aa77239a518a872e8087052019a0d870600a5239
Monil Sorathia <monilsorathia01@gmail.com>
2023-03-30 19:47:48 -0700

    changed to hard coding

--- src/main/java/edu/usc/csci310/project/loginsignup/User.java
+++ src/main/java/edu/usc/csci310/project/loginsignup/User.java
@@ -13,23 +13,23 @@ import java.util.Hashtable;
 import java.util.List;
 
 
-@Entity
-@Table(name = "users")
+//@Entity
+//@Table(name = "users")
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+  //  @Id
+   // @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(name = "name")
+    //@Column(name = "name")
     private String name;
 
-    @Column(name = "email")
+    //@Column(name = "email")
     private String email;
 
-    @Column(name = "password")
+    //@Column(name = "password")
     private String password;
 
-    @Column(name = "watchlists")
+    //@Column(name = "watchlists")
     private Hashtable<String, ArrayList<String>> watchlists;
 
     public User() {}
